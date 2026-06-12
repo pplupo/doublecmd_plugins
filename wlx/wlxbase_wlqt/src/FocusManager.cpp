4cb2b185043e3a381dd3c72870abf243b63b3f07
Peter P. Lupo <pplupo@gmail.com>
2026-06-12 19:16:12 -0400

    Use asynchronous QTimer focus redirection to fix csvview save/edit focus crashes

--- wlx/wlxbase_wlqt/src/FocusManager.cpp
+++ wlx/wlxbase_wlqt/src/FocusManager.cpp
@@ -5,6 +5,7 @@
 #include <QMouseEvent>
 #include <QFocusEvent>
 #include <QChildEvent>
+#include <QPointer>
 #include <QTimer>
 #include <QUndoStack>
 
@@ -77,8 +78,14 @@ void FocusManager::setActive(bool active)
     if (!active) {
         m_activeInput = nullptr;
         m_pluginRoot->clearFocus();
-        if (m_pluginRoot->parentWidget())
-            m_pluginRoot->parentWidget()->setFocus(Qt::OtherFocusReason);
+        if (m_pluginRoot->parentWidget()) {
+            QPointer<QWidget> parent(m_pluginRoot->parentWidget());
+            QTimer::singleShot(0, m_pluginRoot, [parent]() {
+                if (parent) {
+                    parent->setFocus(Qt::OtherFocusReason);
+                }
+            });
+        }
         emit deactivated();
     } else {
         emit activated();
