e6422319f0cd0e6b13ee4be8d6753283ffaea577
Peter P. Lupo <pplupo@gmail.com>
2026-06-15 11:31:47 -0400

    Untrack build directory and binaries in csvview

--- wlx/csvview/build/.qt/QtDeployTargets.cmake
+++
@@ -1,4 +0,0 @@
-set(__QT_DEPLOY_TARGET_wlxbase_wlqt_FILE /home/pplupo/repos/plugins/wlx/csvview/build/wlxbase_wlqt/libwlxbase_wlqt.a)
-set(__QT_DEPLOY_TARGET_wlxbase_wlqt_TYPE STATIC_LIBRARY)
-set(__QT_DEPLOY_TARGET_csvview_qt6_FILE /home/pplupo/repos/plugins/wlx/csvview/build/csvview_qt6.wlx)
-set(__QT_DEPLOY_TARGET_csvview_qt6_TYPE SHARED_LIBRARY)
--- wlx/csvview/build/CMakeFiles/csvview_qt6.dir/csvview_qt6_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/csvview_qt6.dir/csvview_qt6_autogen/mocs_compilation.cpp.o: \
- /home/pplupo/repos/plugins/wlx/csvview/build/csvview_qt6_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
