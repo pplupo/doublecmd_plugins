e5603e3d5e061da402c4430b8bf0511e393671af
Peter P. Lupo <pplupo@gmail.com>
2026-06-30 20:07:07 -0400

    Remove debug prints from structview

--- wlx/structview/src/wlx_entry.cpp
+++ wlx/structview/src/wlx_entry.cpp
@@ -18,24 +18,20 @@ HWND DCPCALL ListLoad(HWND ParentWin, char* FileToLoad, int ShowFlags)
 {
     WLX_TRY {
         Q_UNUSED(ShowFlags);
-        fprintf(stderr, "[structview] ListLoad called: %s\n", FileToLoad);
         fflush(stderr);
 
         if (!QApplication::instance())
             return nullptr;
 
         auto *widget = new StructViewWidget(reinterpret_cast<QWidget*>(ParentWin));
-        fprintf(stderr, "[structview] Widget created, calling loadFile\n");
         fflush(stderr);
 
         if (!widget->loadFile(QString::fromUtf8(FileToLoad))) {
-            fprintf(stderr, "[structview] loadFile FAILED for: %s\n", FileToLoad);
             fflush(stderr);
             delete widget;
             return nullptr;
         }
 
-        fprintf(stderr, "[structview] loadFile OK, showing widget\n");
         fflush(stderr);
         widget->show();
         return reinterpret_cast<HWND>(widget);
@@ -46,7 +42,6 @@ HWND DCPCALL ListLoad(HWND ParentWin, char* FileToLoad, int ShowFlags)
 void DCPCALL ListCloseWindow(HWND ListWin)
 {
     WLX_TRY {
-        fprintf(stderr, "[structview] ListCloseWindow\n");
         fflush(stderr);
         auto *widget = reinterpret_cast<StructViewWidget*>(ListWin);
         delete widget;
